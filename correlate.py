f666fce45dc39320b4c60fbcb1de827e49e35608
christianreiser <reisercg@gmail.com>
2019-09-17 14:08:43 +0200

    visualize correlation matrix

--- correlate.py
+++ correlate.py
@@ -1,32 +1,61 @@
+import numpy as np
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import os
 
 """
 correlate steps and min active
 
 load data form json, into df, correlation matrix, visualize
 """
-
-def json_2_1_feature_df(feature, json_file_path):
+path_to_json_files = './exist0/'
+def json_2_1_feature_df(json_root, json_file):
     """
     as name says
     """
-    df = pd.read_json(json_file_path)  # read json to df
-    df = df.rename(columns={"value": feature})  # rename columnname value to steps
+    feature = os.path.splitext(json_file)[0]
+    #print(json_root, json_file)
+    df = pd.read_json(os.path.join(json_root, json_file))  # read json to df
+    df = df.rename(columns={"value": feature})  # rename columnname value to feature
     df = df.set_index('date')  # set date as index
     return df
 
-# json_2_1_feature_df
-df_steps = json_2_1_feature_df("steps", './exist0/steps.json')
-df_steps_active_min = json_2_1_feature_df("steps_active_min", './exist0/steps_active_min.json')
-
-# join dfs
-df_joined = df_steps.join(df_steps_active_min)
+# create df from json files 
+for root, dirs, files in os.walk(path_to_json_files):
+    i=0
+    for file in files:
+        if file.endswith(".json"):
+            if i == 0:
+                df_0 = json_2_1_feature_df(root, file)
+            elif i == 1:
+                df_1 = json_2_1_feature_df(root, file)
+                df = df_0.join(df_1)
+            else:
+                df_1 = json_2_1_feature_df(root, file)
+                df = df.join(df_1)
+            i+=1
 
 # correlate
-corr_matrix = df_joined.corr()
+corr_matrix = df.corr()
+# corr_matrix = np.fill_diagonal(corr_matrix.values, -1)  # drop selfcorrelation but doesnt work
+# print(corr_matrix)
 
-print(df_joined)
-print(corr_matrix)
+
+# sort correlation matrix
+s = corr_matrix.unstack()
+so = s.sort_values(kind="quicksort")
+print(so)
+
+
+# plot
+f = plt.figure(figsize=(19, 15))
+plt.matshow(corr_matrix, fignum=f.number)
+plt.xticks(range(df.shape[1]), df.columns, fontsize=7, rotation=90)
+plt.yticks(range(df.shape[1]), df.columns, fontsize=7)
+cb = plt.colorbar()
+cb.ax.tick_params(labelsize=7)
+plt.title('Correlation Matrix', fontsize=12);
+plt.show()
+
+
