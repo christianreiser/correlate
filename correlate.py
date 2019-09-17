c2675dd3b86661a71929037c0bcea8fa2ac31544
christianreiser <reisercg@gmail.com>
2019-09-17 10:39:07 +0200

    correlation matrix

--- correlate.py
+++ correlate.py
@@ -8,30 +8,6 @@ correlate steps and min active
 
 load data form json, into df, correlation matrix, visualize
 """
-value_steps_active_min = []
-date_steps_active_min = []
-value_steps = []
-date_steps = []
-
-with open('./exist0/steps.json') as json_file:
-    steps_loaded = json.load(json_file)
-    for entry in steps_loaded:
-        value_steps.append(entry['value'])
-        date_steps.append(entry['date'])
-
-with open('./exist0/steps_active_min.json') as json_file:
-    steps_loaded = json.load(json_file)
-    for entry in steps_loaded:
-        value_steps_active_min.append(entry['value'])
-        date_steps_active_min.append(entry['date'])
-
-#plt.plot(steps, steps_active_min, 'ro')
-#plt.show()
-
-df_steps = pd.DataFrame(value_steps, date_steps)
-df_steps_active_min = pd.DataFrame(value_steps_active_min, date_steps_active_min)
-#print(df_steps)
-#print(df_steps_active_min)
 
 def json_2_1_feature_df(feature, json_file_path):
     """
@@ -42,5 +18,15 @@ def json_2_1_feature_df(feature, json_file_path):
     df = df.set_index('date')  # set date as index
     return df
 
-df = json_2_1_feature_df("steps", './exist0/steps.json')
-print(df)
+# json_2_1_feature_df
+df_steps = json_2_1_feature_df("steps", './exist0/steps.json')
+df_steps_active_min = json_2_1_feature_df("steps_active_min", './exist0/steps_active_min.json')
+
+# join dfs
+df_joined = df_steps.join(df_steps_active_min)
+
+# correlate
+corr_matrix = df_joined.corr()
+
+print(df_joined)
+print(corr_matrix)
