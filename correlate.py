e479a9edb57cbe038f995862df1ff78d18ec4294
christianreiser <reisercg@gmail.com>
2019-09-17 10:22:29 +0200

    json to df

--- correlate.py
+++ correlate.py
@@ -1,11 +1,44 @@
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 """
 correlate steps and min active
+
+load data form json, into df, correlation matrix, visualize
 """
+value_steps_active_min = []
+date_steps_active_min = []
+value_steps = []
+date_steps = []
 
 with open('./exist0/steps.json') as json_file:
     steps_loaded = json.load(json_file)
     for entry in steps_loaded:
-        print(entry['value'])
-        print(entry['date'])
+        value_steps.append(entry['value'])
+        date_steps.append(entry['date'])
+
+with open('./exist0/steps_active_min.json') as json_file:
+    steps_loaded = json.load(json_file)
+    for entry in steps_loaded:
+        value_steps_active_min.append(entry['value'])
+        date_steps_active_min.append(entry['date'])
+
+#plt.plot(steps, steps_active_min, 'ro')
+#plt.show()
+
+df_steps = pd.DataFrame(value_steps, date_steps)
+df_steps_active_min = pd.DataFrame(value_steps_active_min, date_steps_active_min)
+#print(df_steps)
+#print(df_steps_active_min)
+
+#def json_2_1_feature_df(feature, json_file_path):
+"""
+"""
+dftest = pd.read_json('./exist0/steps.json')  # read json to df
+dftest = dftest.rename(columns={"value": "steps"})  # rename columnname value to steps
+dftest = dftest.set_index('date')  # set date as index
+print(dftest)
+
+#json_2_1_feature_df(steps, './exist0/steps.json'
