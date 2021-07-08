98d730c1ed0df0d254fc7de1dd389c310dd06f7d
christianreiser <reisercg@gmail.com>
2021-07-08 15:48:32 +0200

    filteredRunVO2Max

--- MyFitbitData/fitbit_vo2max_json_to_csv.py
+++ MyFitbitData/fitbit_vo2max_json_to_csv.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 
+# TODO get from run_vo2_max-2019-02-12.json
+
 path_to_json_files = '/home/chrei/PycharmProjects/correlate/MyFitbitData/ChrisRe/Physical Activity'
 output_filename = 'fitbit_vo2max.csv'
 verbose = True
@@ -32,46 +34,39 @@ for root, dirs, files in os.walk(path_to_json_files):
         # exclude files small files to reduce noise
         # exclude correlations.json
         file_size = os.stat(os.path.join(path_to_json_files, file))[6]
-        if file.endswith(".json") and file.startswith('exercise-') and file not in excludedFiles:
+        if file.endswith(".json") and file.startswith('run_vo2_max-') and file not in excludedFiles:
 
             df = pd.read_json(os.path.join(root, file))  # read json to d
-            # df = df.set_index('originalStartTime')  # set date as index
+            if coldStart:
+                all_df = df
+                coldStart = False
+            else:
+                all_df = all_df.append(df)
 
-            # all_files_df = pd.DataFrame(columns=['originalStartTime', 'vo2Max', 'originalDuration'])
-            if 'vo2Max' in df:
-                df = df.dropna()
-                df_target = df['originalStartTime']
-                # test = df['vo2Max']
-                # vo2Max = [d.get('vo2Max') for d in df['vo2Max'].dic]
+filteredRunVO2MaxList = []
+dateList = []
+for i, row in all_df.iterrows():
+    filteredRunVO2MaxList.append(round(row.value['filteredRunVO2Max'], 5))
+    dateList.append(row.dateTime)
 
-                df_target = pd.concat([df_target, df['vo2Max']], axis=1)
-                # df_target = pd.concat([df_target, df['originalDuration']], axis=1)
-                if coldStart:
-                    all_files_df = df_target
-                    coldStart = False
-                else:
-                    all_files_df = all_files_df.append(df_target)
-
-# extract from dict
-all_files_df['vo2max'] = [d.get('vo2Max') for d in all_files_df['vo2Max']]
-all_files_df = all_files_df.drop('vo2Max', axis=1)
+all_files_df = pd.DataFrame(dateList, columns=['dateTime'])
+all_files_df['filteredRunVO2Max'] = filteredRunVO2MaxList
 
 # aggregate when multiple per day
-all_files_df['originalStartTime'] = pd.to_datetime(all_files_df['originalStartTime'])
-all_files_df = all_files_df.groupby(all_files_df['originalStartTime'].dt.date).mean()
+all_files_df['dateTime'] = pd.to_datetime(all_files_df['dateTime'])
+all_files_df = all_files_df.groupby(all_files_df['dateTime'].dt.date).mean()
 
 # round
-all_files_df.vo2max = round(all_files_df.vo2max, 0)
+all_files_df.filteredRunVO2Max = round(all_files_df.filteredRunVO2Max, 1)
 
 # fill missing dates with NaN
 all_files_df.reset_index(level=0, inplace=True)
-firstDate = all_files_df['originalStartTime'][0]
-lastDate = all_files_df['originalStartTime'].iloc[-1]
-all_files_df = all_files_df.set_index('originalStartTime')  # set date as index
+firstDate = datetime.strptime('2019/02/11', '%Y/%m/%d')  # all_files_df['dateTime'][0]
+lastDate = all_files_df['dateTime'].iloc[-1]
+all_files_df = all_files_df.set_index('dateTime')  # set date as index
 idx = pd.date_range(firstDate, lastDate)
 all_files_df.index = pd.DatetimeIndex(all_files_df.index)
 all_files_df = all_files_df.reindex(idx, fill_value=float("NaN"))
-
 
 # write file
 all_files_df.to_csv(output_filename)
