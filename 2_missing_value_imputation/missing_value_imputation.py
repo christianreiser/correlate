9100d85cca58a64c0cdc9b9aae0ea6247a73389f
christianreiser <reisercg@gmail.com>
2021-07-15 10:32:39 +0200

    linear interpolation

--- 2_missing_value_imputation/missing_value_imputation.py
+++ 2_missing_value_imputation/missing_value_imputation.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pandas as pd
 
 target_label = 'mood'
@@ -6,8 +8,8 @@ df = pd.read_csv('/home/chrei/code/quantifiedSelfData/daily_summaries.csv', inde
 print(df.columns)
 
 # drop gps location
-df = df.drop(
-    ['Low latitude (deg)', 'Low longitude (deg)', 'High latitude (deg)', 'High longitude (deg)'], axis=1)
+# df = df.drop(
+#     ['Low latitude (deg)', 'Low longitude (deg)', 'High latitude (deg)', 'High longitude (deg)'], axis=1)
 
 # drop nutrition
 df = df.drop(
@@ -15,14 +17,21 @@ df = df.drop(
 
 # 'sodium', 'fat', 'carbohydrates', 'protein', 'fibre', 'kcal_in',
 
+# interpolate weight and vo2 max linearly
+df['weight'] = df['weight'].interpolate(method='linear')
+df['VO2Max'] = df['VO2Max'].interpolate(method='linear')
+
+# drop days where too much data is missing
+datelist = pd.date_range(datetime.strptime('2019-02-11', '%Y-%m-%d'), periods=615).tolist()  #
+datelist = datelist + pd.date_range(datetime.strptime('2021-06-16', '%Y-%m-%d'), periods=41).tolist()
+datelist = [day.strftime('%Y-%m-%d') for day in datelist]
+df = df.drop(datelist, axis=0)
+
 # drop days without mood rating
 for day, _ in df.iterrows():
     # print('day',day)
     if df[target_label][day] != df[target_label][day]:  # checks for NaN
         df = df.drop(day)
-
-# drop days where too much data is missing
-df = df.drop(['2019-02-11', '2019-02-12', '2019-02-12', '2019-02-13'])
 
 # fill missing values with mean value
 # get mean value
@@ -32,7 +41,7 @@ for attribute_name in df.columns:
     nan_data_true_false = pd.isnull(df[attribute_name])
     nan_numeric_indices = pd.isnull(df[attribute_name]).to_numpy().nonzero()[0]
     nan_dates = nan_data_true_false[nan_numeric_indices].index
-    if len(nan_dates) >0:
+    if len(nan_dates) > 0:
         atest = nan_dates
         print('d')
     # for nan_date in nan_dates:
